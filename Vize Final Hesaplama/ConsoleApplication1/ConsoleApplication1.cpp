d5b6c518193ae34ceffc21a3aa4d8302d450bbb3
Eren Akpınar <orijinalerenakpinar@gmail.com>
2025-02-09 18:12:03 +0300

    Add files via upload

---
+++ Vize Final Hesaplama/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <locale.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+   
+    string isim;
+    double vize, final, sonuc;
+
+    cout << "İsminizi Giriniz: ";
+    cin >> isim;
+    cout << "Vize notunuzu giriniz: ";
+    cin >> vize;
+    cout << "Final notunuzu giriniz: ";
+    cin >> final;
+    sonuc = vize * 40 / 100 + final * 60 / 100;
+    
+    if (sonuc>=50)
+    {
+        cout << "Tebrikler " << isim << " Geçtiniz.";
+
+    }
+    else
+    {
+        cout << "Maalesef " << isim << " Kaldınız.";
+    }
+}
+
+
